f91111313c72ae0f2e17262fa5ad499c8af3fc9e
Tanmay Thag <59114229+tanmaythag@users.noreply.github.com>
2020-09-08 19:11:59 +0530

    Create Day1letandConst.js

---
+++ Day1letandConst.js
@@ -0,0 +1,11 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r =readLine();
+    const PI = Math.PI;
+    var area =  PI*r*r;
+    var perimeter = 2*r*PI;
+    
+    // Print the area of the circle:
+    console.log(area);
+    // Print the perimeter of the circle:
+    console.log(perimeter);
